18c53475613c0bb76d935d97d6a309376381474f
Lee Broxson <broxsonl@gmail.com>
2016-08-31 13:46:38 -0700

    Server.js updated.

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var express = require('express'),
 
 // NOTE: now use our proxy within a function to request
 //        our github data on the server.
-var proxyGithub = function(request, response) {
+var proxyGitHub = function(request, response) {
   console.log('Routing GitHub request for ', request.params[0]);
   (requestProxy({
     url: 'https://api.github.com/' + request.params[0],
