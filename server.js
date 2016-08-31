9af4703fbf08a7ac76ae80d1dac71bfde198b8e6
KBrandtjen <Keithbrandtjen09@gmail.com>
2016-08-31 13:42:22 -0700

    added one line on server.js

--- server.js
+++ server.js
@@ -1,20 +1,21 @@
 var express = require('express'),
   // NOTE: require in our request proxy module
-
+  requestProxy = require('express-request-proxy'),
   port = process.env.PORT || 3000,
   app = express();
 
 // NOTE: now use our proxy within a function to request
 //        our github data on the server.
 var proxyGitHub = function(request, response) {
-    console.log('Routing GitHub request for ', request.params[0]);
-    (requestProxy({
-      url: 'https'
-    }))
-}
+  console.log('Routing GitHub request for ', request.params[0]);
+  (requestProxy({
+    url: 'https://api.github.com/' + request.params[0],
+    headers: {Authorization: 'token ' + process.env.GITHUB_TOKEN}
+  }))(request, repsonse);
+};
 
+app.get('/github/*', proxyGitHub);
 app.use(express.static('./'));
-
 app.get('*', function(request, response) {
   console.log('New request:', request.url);
   response.sendFile('index.html', { root: '.' });
