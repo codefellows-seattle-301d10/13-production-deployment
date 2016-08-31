6ae16d358ae55172655373457250dd56ebb2970e
Rachael Wisecarver <rachael.wisecarver@gmail.com>
2016-08-31 16:15:24 -0700

    debugging the repo.js file

--- scripts/models/repo.js
+++ scripts/models/repo.js
@@ -1,32 +1,28 @@
 (function(module) {
   var reposObj = {};
-
   reposObj.allRepos = [];
   reposObj.followers = [];
-
   reposObj.requestRepos = function(callback) {
     // NOTE: refactor this request into an $.ajax call
     $.when(
-     $.get('/github.com/users/codefellows-seattle-301d9/repos' +
+     $.get('/github/users/codefellows-seattle-301d9/repos' +
             '?per_page=10' +
-            '&sort=updated').done(function(data){
-              // NOTE: since the 'data' paramter comes back as an
-              // array of objects, we can reassign allRepos below.
+            '&sort=updated')
+            .done(function(data) {
               reposObj.allRepos = data;
             }),
-     $.get('/github.com/users/patci/followers' +
+     $.get('/github/users/patci/followers' +
             '?per_page=5' +
-            '&sort=updated').done(function(data) {
+            '&sort=updated')
+            .done(function(data) {
               reposObj.followers = data;
             })
-          ).done(callback);
+    ).done(callback);
   };
-
   reposObj.withTheAttribute = function(attr) {
     return reposObj.allRepos.filter(function(aRepo) {
       return aRepo[attr];
     });
   };
-
   module.reposObj = reposObj;
 })(window);
