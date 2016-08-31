e1d264804d0ffc88e5f12b841ba78462b9f4138c
Britt Dawn <brittdawndev@gmail.com>
2016-08-31 13:15:12 -0700

    Updating server files after Type Error.

--- scripts/models/repo.js
+++ scripts/models/repo.js
@@ -1,32 +1,28 @@
 (function(module) {
   var reposObj = {};
-
   reposObj.allRepos = [];
   reposObj.followers = [];
-
   reposObj.requestRepos = function(callback) {
     // NOTE: refactor this request into an $.ajax call
     $.when(
-     $.get('/github.com/users/codefellows-seattle-301d10/repos' +
+     $.get('/github/users/codefellows-seattle-301d9/repos' +
             '?per_page=10' +
             '&sort=updated')
             .done(function(data) {
               reposObj.allRepos = data;
             }),
-     $.get('/github.com/users/patci/followers' +
+     $.get('/github/users/patci/followers' +
             '?per_page=5' +
             '&sort=updated')
             .done(function(data) {
               reposObj.followers = data;
             })
-      ).done(callback);
+    ).done(callback);
   };
-
   reposObj.withTheAttribute = function(attr) {
     return reposObj.allRepos.filter(function(aRepo) {
       return aRepo[attr];
     });
   };
-
   module.reposObj = reposObj;
 })(window);
