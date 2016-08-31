45d0f79ffd68f77a1ec579507bbe3438524f7915
Britt Dawn <brittdawndev@gmail.com>
2016-08-31 12:07:30 -0700

    update server for proxy and refactor client request

--- scripts/models/repo.js
+++ scripts/models/repo.js
@@ -7,29 +7,20 @@
   reposObj.requestRepos = function(callback) {
     // NOTE: refactor this request into an $.ajax call
     $.when(
-     $.ajax({
-       url: 'https://api.github.com/users/codefellows-seattle-301d9/repos' +
+     $.get('https://api.github.com/users/codefellows-seattle-301d10/repos' +
             '?per_page=10' +
-            '&sort=updated',
-       type: 'GET',
-       headers: { 'Authorization': 'token ' + githubToken },
-       success: function(data) {
-         // NOTE: since the 'data' paramter comes back as an
-         // array of objects, we can reassign allRepos below.
-         reposObj.allRepos = data;
-       }
-     }),
-     $.ajax({
-       url: 'https://api.github.com/users/patci/followers' +
+            '&sort=updated')
+            .done(function(data) {
+              reposObj.allRepos = data;
+            }),
+     $.get('https://api.github.com/users/patci/followers' +
             '?per_page=5' +
-            '&sort=updated',
-       type: 'GET',
-       headers: { 'Authorization': 'token ' + githubToken },
-       success: function(data) {
-         reposObj.followers = data;
-       }
-     })
-    ).done(callback);
+            '&sort=updated')
+            .done(function(data) {
+              reposObj.followers = data;
+            })
+      ).done(callback);
+    };
   };
 
   reposObj.withTheAttribute = function(attr) {
