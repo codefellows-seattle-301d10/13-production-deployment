aa048151a5c6d7143bdf646de1f28393b97c462c
Rachael Wisecarver <rachael.wisecarver@gmail.com>
2016-08-31 15:50:17 -0700

    reconfiguring repo.js ajax function into a get function

--- scripts/models/repo.js
+++ scripts/models/repo.js
@@ -7,29 +7,19 @@
   reposObj.requestRepos = function(callback) {
     // NOTE: refactor this request into an $.ajax call
     $.when(
-     $.ajax({
-       url: 'https://api.github.com/users/codefellows-seattle-301d9/repos' +
+     $.get('/github.com/users/codefellows-seattle-301d9/repos' +
             '?per_page=10' +
-            '&sort=updated',
-       type: 'GET',
-       headers: { 'Authorization': 'token ' + githubToken },
-       success: function(data) {
-         // NOTE: since the 'data' paramter comes back as an
-         // array of objects, we can reassign allRepos below.
-         reposObj.allRepos = data;
-       }
-     }),
-     $.ajax({
-       url: 'https://api.github.com/users/patci/followers' +
+            '&sort=updated').done(function(data){
+              // NOTE: since the 'data' paramter comes back as an
+              // array of objects, we can reassign allRepos below.
+              reposObj.allRepos = data;
+            }),
+     $.get('/github.com/users/patci/followers' +
             '?per_page=5' +
-            '&sort=updated',
-       type: 'GET',
-       headers: { 'Authorization': 'token ' + githubToken },
-       success: function(data) {
-         reposObj.followers = data;
-       }
-     })
-    ).done(callback);
+            '&sort=updated').done(function(data) {
+              reposObj.followers = data;
+            })
+          ).done(callback);
   };
 
   reposObj.withTheAttribute = function(attr) {
